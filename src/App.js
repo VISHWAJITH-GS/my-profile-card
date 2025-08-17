ca629fac65d46c22c605474be7ef813c7fa59687
VISHWAJITH006 <vishwajithgs@student.tce.edu>
2025-08-17 13:46:37 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ProfileCard from './components/ProfileCard';
+import LetterGlitch from './LetterGlitch'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ 
+      position: 'relative', 
+      width: '100vw', 
+      height: '100vh',
+      overflow: 'hidden'
+    }}>
+      {/* Background Layer */}
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: 1
+      }}>
+        <LetterGlitch
+          glitchColors={['#2b4539', '#61dca3', '#61b3dc']}
+          glitchSpeed={50}
+          centerVignette={true}
+          outerVignette={false}
+          smooth={true}
+        />
+      </div>
+
+      {/* Content Layer */}
+      <div style={{
+        position: 'relative',
+        zIndex: 2,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        padding: '20px'
+      }}>
+        <ProfileCard
+          name="VISHWAJTIH"
+          title="AIML Developer"
+          handle="Ajith"
+          status="Online"
+          contactText="Contact Me"
+          avatarUrl="/avatar.jpg"
+          miniAvatarUrl="/avatar.jpg"
+          enableTilt={true}
+          enableMobileTilt={false}
+          onContactClick={() => window.open('https://vishwajith-gs.github.io/portfolio/', '_blank')}
+        />
+      </div>
     </div>
   );
 }
